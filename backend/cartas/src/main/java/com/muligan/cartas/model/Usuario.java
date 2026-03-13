8507c14a6cd6b52dae55f4d1fbf46b15faf63a48
Ablagom03 <1eso1516ablagom@gmail.com>
2026-03-13 13:50:56 +0100

    Empezado usuario

--- backend/cartas/src/main/java/com/muligan/cartas/model/Usuario.java
+++ backend/cartas/src/main/java/com/muligan/cartas/model/Usuario.java
@@ -11,4 +11,47 @@ public class Usuario {
     private String nombreUsr;
     private String email;
     private String passwd;
+
+    public Usuario(){
+
+    }
+
+    public Usuario(Integer usrId, String nombreUsr, String email, String passwd){
+        UsrId = usrId;
+        this.nombreUsr = nombreUsr;
+        this.email = email;
+        this.passwd = passwd;
+    }
+
+    public Integer UsrId(){
+        return this.UsrId;
+    }
+    
+    public void setUsrId(Integer UsrId){
+        this.Cardid = Cardid;
+    }
+
+    public String getNombreUsr(){
+        return this.nombreUsr;
+    }
+
+    public void setNombreUsr(String nombreUsr){
+        this.nombreUsr = nombreUsr;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public String getPasswd(){
+        return this.passwd;
+    }
+
+    public String setPasswd(String passwd){
+        this.passwd = passwd;
+    }
 }

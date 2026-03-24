54a480cc5c1f77b6d34fa83bd7ef82bfc983bbf1
DanielGZamora <garzizamora@gmail.com>
2026-03-24 22:20:49 +0100

    ordenando un pelin

--- frontend/angularfront/src/app/app.routes.ts
+++ frontend/angularfront/src/app/app.routes.ts
@@ -1,14 +1,11 @@
 import { Routes } from '@angular/router';
 import { ListaCartasComponent } from './component/lista-cartas/lista-cartas.component';
 import { LoginComponent } from './component/login/login.component';
-import { PaginaInicioComponent } from './component/pagina-inicio/pagina-inicio.component';
 import { NotFoundComponent } from './component/not-found/not-found.component';
 
 
 export const routes: Routes = [
     { path: 'cartas', component: ListaCartasComponent },
     { path: 'login', component: LoginComponent},
-    { path: 'inicio', component: PaginaInicioComponent},
-
     { path: '404', component: NotFoundComponent}
 ];

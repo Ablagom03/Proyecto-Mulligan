6aec1e8f6539e982a2b6d5faa836035810b1ce98
Ablagom03 <1eso1516ablagom@gmail.com>
2026-04-15 14:32:24 +0200

    Funcionalidad en usuario para cambiar listas y algunos pequeños cambios de estilos

---
+++ frontend/angularfront/src/app/component/usuario/lista-deseados/lista-deseados.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-deseados',
+  templateUrl: './lista-deseados.component.html',
+  styleUrls: ['./lista-deseados.component.css']
+})
+export class ListaDeseadosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- frontend/angularfront/src/app/component/usuario/usuario.component.ts
+++ frontend/angularfront/src/app/component/usuario/usuario.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { ListaInventarioComponent } from './lista-inventario/lista-inventario.component';
+import { ListaDeseadosComponent } from './lista-deseados/lista-deseados.component'; 
 
 @Component({
   selector: 'app-usuario',
   templateUrl: './usuario.component.html',
+  imports: [ListaInventarioComponent, ListaDeseadosComponent],
   styleUrls: ['./usuario.component.css']
 })
 export class UsuarioComponent implements OnInit {
@@ -12,4 +15,14 @@ export class UsuarioComponent implements OnInit {
   ngOnInit() {
   }
 
+  //Propiedad para comprobar la vista actual, cuál lista se ve
+  vistaActual: 'inventario' | 'deseados' = 'inventario';
+
+  mostrarInventario() {
+    this.vistaActual = 'inventario';
+  }
+
+  mostrarDeseados() {
+    this.vistaActual = 'deseados';
+  }
 }

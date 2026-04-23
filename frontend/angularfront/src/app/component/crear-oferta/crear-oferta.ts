d2b93b60144ec71917407e877d4d7df7e1cbe99b
DanielGZamora <garzizamora@gmail.com>
2026-04-23 18:35:55 +0200

    small detail

--- frontend/angularfront/src/app/component/crear-oferta/crear-oferta.ts
+++ frontend/angularfront/src/app/component/crear-oferta/crear-oferta.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-crear-oferta',
   imports: [],
+  standalone: true,
   templateUrl: './crear-oferta.html',
   styleUrl: './crear-oferta.css',
 })
-export class CrearOferta {}
+export class CrearOferta { 
+  
+}

3f78365af510bacbae132a246015234fad72b6af
Ablagom03 <1eso1516ablagom@gmail.com>
2026-04-27 18:57:57 +0200

    Añadido el buscador de cartas

--- frontend/angularfront/src/app/component/pagina-inicio/pagina-inicio.component.ts
+++ frontend/angularfront/src/app/component/pagina-inicio/pagina-inicio.component.ts
@@ -1,16 +1,56 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CartasService } from '../../service/cartas.service';
+import { Carta } from '../../model/Carta';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common'
+
 
 @Component({
   selector: 'app-pagina-inicio',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, FormsModule, CommonModule],
   templateUrl: './pagina-inicio.component.html',
   styleUrls: ['./pagina-inicio.component.css']
 })
 export class PaginaInicioComponent implements OnInit {
 
+  busqueda: string = '';
+  resultados: Carta[] = [];
+  datos: Carta[] = [];
+
+  constructor(private cartasService: CartasService) { }
+
   ngOnInit() {
+    this.cartasService.getCartas().subscribe((cartas: Carta[]) => {
+      this.datos = cartas.map(carta => carta);
+    });
   }
 
+  buscarCartas() {
+    this.resultados = this.datos.filter(carta =>
+      carta.nombrecard.toLowerCase().includes(this.busqueda.toLowerCase())
+    );
+
+    this.mostrar = true;
+
+    //Para debuguear
+    console.log("Busqueda: ", this.busqueda);
+    console.log("Datos: ", this.datos);
+    console.log("Resultado: ", this.resultados);
+  }
+
+  //Para el dropdown
+  mostrar: boolean = false;
+
+  seleccionar(carta: string) {
+    this.busqueda = carta;
+    this.mostrar = false;
+  }
+
+  ocultar() {
+    setTimeout(() => {
+      this.mostrar = false;
+    }, 3);
+  }
 }
